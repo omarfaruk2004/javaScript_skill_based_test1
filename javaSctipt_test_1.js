33e7984baceac50accca691b26402641bcac6009
omarfaruk <farukmd2004@gmail.com>
2022-07-28 12:53:29 -0500

    final

--- javaSctipt_test_1.js
+++ javaSctipt_test_1.js
@@ -40,4 +40,38 @@ console.log("Your total is: ", price);
 
 // Coding Challenge #3: What’s for dinner?
 
+function randomRestaurant(numOfRestaurants) {
+  const restaurantArr = [
+    "Applebees",
+    "Panera Bread",
+    "Popeyes",
+    "Chopt",
+    "Chick-fil-A",
+    "Cheesecake Factory",
+    "Five Guys",
+  ];
+  const resArrLen = restaurantArr.length;
+
+  const randomRastaurantList = [];
+  for (let i = 0; i < numOfRestaurants; i++) {
+    const randomNum = Math.floor(Math.random() * resArrLen);
+    randomRastaurantList.push(restaurantArr[randomNum]);
+  }
+  return randomRastaurantList;
+}
+
+const resultArr = randomRestaurant(3);
+console.log("the arr is:", resultArr);
+
 //Coding Challenge #4: Leap Year
+function isLeapYear(year) {
+  if (year % 4 === 0 && year % 100 !== 0) {
+    return true;
+  } else if (year % 400 === 0) {
+    return true;
+  }
+  return false;
+}
+
+const leapYear = isLeapYear(2996);
+console.log("is lear year? =>", leapYear);
